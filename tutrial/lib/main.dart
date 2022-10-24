8656ca6e50919a12b1a854dcf42d97d737564826
Toma-0 <toma0122yukako@icloud.com>
2022-10-24 18:51:01 +0900

    stlessを行う

--- tutrial/lib/main.dart
+++ tutrial/lib/main.dart
@@ -4,23 +4,25 @@ import 'package:flutter/material.dart';
 
 //main関数は全てのflutterアプリの出発点です。
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 209, 209, 209),
-
         appBar: AppBar(
           title: Text("I AM Rich"),
-          backgroundColor: Colors.grey,
+          backgroundColor: Colors.red,
         ),
-      
-         body: Center(
+        body: Center(
           child: Image(
             image: AssetImage('images/TEST.png'),
           ),
         ),
-        
       ),
-    ),
-  );
+    );
+  }
 }
