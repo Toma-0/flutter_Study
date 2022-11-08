0a0cd955da7bf0fe70610a1b1ff4a6ea6c0b4fd4
Toma-0 <toma0122yukako@icloud.com>
2022-11-08 14:59:55 +0900

    セクション7 Dicee

---
+++ dice/lib/main.dart
@@ -0,0 +1,26 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text("Dicee"),
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [Image(image: AssetImage("images/dice1.png"))],
+    );
+  }
+}
